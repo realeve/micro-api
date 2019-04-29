a437cf2d63512ec8a318192ab0a1f9be979ce0c5
realeve@qq.com <realeve@qq.com>
2019-04-29 16:51:57 +0800

    :memo: 更新每秒请求数据限制

--- src/app.js
+++ src/app.js
@@ -8,6 +8,23 @@ const fastify = require('fastify')({
   // logger: false
 });
 
+// 超时限制
+fastify.register(require('fastify-rate-limit'), {
+  max: 200, // default 1000
+  timeWindow: 60 * 1000, // default 1000 * 60
+  cache: 5000, // default 5000
+  whitelist: ['127.0.0.1'], // default []
+  // redis: new Redis({ host: '127.0.0.1' }), // default null
+  skipOnError: true, // default false
+  keyGenerator: function(req) {
+    return (
+      req.headers['x-real-ip'] || // nginx
+      req.headers['x-client-ip'] || // apache
+      req.raw.ip
+    ); // fallback to default
+  }
+});
+
 fastify.register(require('fastify-mysql'), {
   promise: true,
   connectionString: `mysql://${config.user}:${config.password}@${config.host}:${
@@ -18,6 +35,8 @@ fastify.register(require('fastify-mysql'), {
 fastify.register(AutoLoad, {
   dir: path.join(__dirname, 'services')
 });
+
+// fastify.register(require('fastify-response-time'));
 
 fastify.listen(3000, '0.0.0.0', (err, address) => {
   if (err) throw err;
