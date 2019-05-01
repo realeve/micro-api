2be797eb9c16bd5a7df365437b40d568f81561d1
realeve@qq.com <realeve@qq.com>
2019-05-02 01:57:26 +0800

    :bug: 消息依次消费，不重复消费

--- src/services/mq/consumer.js
+++ src/services/mq/consumer.js
@@ -1,35 +1,48 @@
 const { kafka: config } = require('../../config/kafka');
 const kafka = require('kafka-node'),
   Consumer = kafka.Consumer,
-  client = new kafka.KafkaClient(config);
+  client = new kafka.KafkaClient(config),
+  logger = require('kafka-node/lib/logging')('kafka-node:Consumer');
 
-var logger = require('kafka-node/lib/logging')('kafka-node:Consumer');
+let payload = [{ topic: 'test' }, { topic: 'demo' }, { topic: 'topiccc' }];
+let consumer;
 
-const consumer = new Consumer(
-  client,
-  [
-    {
-      topic: 'topiccc'
-    },
-    {
-      topic: 'demo'
-    }
-  ],
-  {
-    autoCommit: false
-  }
-);
+// 使用自动获取payload，需要将集群所有节点启动
+const admin = new kafka.Admin(client);
+admin.listTopics((err, res) => {
+  // console.log('topics', res);
+  res = res.filter((item) => item.metadata).map(({ metadata }) => metadata);
+  res = Object.keys(res[0]).filter((item) => item !== '__consumer_offsets');
+  payload = res.map((topic) => ({ topic }));
+  console.log(payload);
+});
 
-consumer.on('message', function(message) {
-  console.log('开始消费:', new Date());
+consumer = new Consumer(client, payload, {
+  autoCommit: false
+  // fetchMaxWaitMs: 30000
+});
+
+consumer.on('message', async function(message) {
+  // console.log('开始消费:', new Date());
   let { topic, value, offset, partition, highWaterOffset: totalMsg } = message;
-  console.log({ topic, value, offset, totalMsg });
-  consumer.commit(true, function(err, data) {
-    err && logger.debug('auto commit offset', err);
-    consumer.setOffset(topic, partition, offset + 1);
-    console.log(data);
-    console.log(topic, partition, offset + 1);
+  // console.log(new Date(), offset);
+  let res = await new Promise((resolve) => {
+    setTimeout(() => {
+      // console.log(`start ${offset}.${value}`);
+      consumer.commit(true, function(err, data) {
+        err && logger.debug('auto commit offset', err);
+        resolve(`${offset}/${totalMsg - 1}.`);
+      });
+    }, 1000);
   });
+  console.info(res);
+  // setTimeout(() => {
+  //   console.log(`start ${offset}.${value}`);
+  //   consumer.commit(true, function(err, data) {
+  //     err && logger.debug('auto commit offset', err);
+  //     console.log(`${offset} is complete.`);
+  //   });
+  // }, 2000);
 });
 
 consumer.on('error', function(err) {
